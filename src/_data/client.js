4012339c3243cc54768ec4df96ce92e0565b2504
Fraser Wilkinson <fraserwilk@users.noreply.github.com>
2024-06-20 22:07:06 +1000

    add dronework content

--- src/_data/client.js
+++ src/_data/client.js
@@ -1,20 +1,20 @@
 module.exports = {
-    "name": "Code Stitch Web Designs",
-    "email": "help@codestitch.app",
+    "name": "Fission Digital Flyovers",
+    "email": "help@fission.digital",
     "phoneForTel": "555-779-4407",
     "phoneFormatted": "(555) 779-4407",
     "address": {
         "lineOne": "First Address Line",
         "lineTwo": "Second Address Line",
-        "city": "Denver",
-        "state": "CO",
-        "zip": "80206",
-        "mapLink": "https://goo.gl/maps/UAQn4vuGDiwv7DV39"
+        "city": "Caulfield East",
+        "state": "VIC",
+        "zip": "3145",
+        "mapLink": "https://maps.app.goo.gl/xG6ZkMjtMyywVAry5"
     },
     "socials": {
         "facebook": "https://www.facebook.com/",
         "instagram": "https://www.instagram.com/",
     },
     //! Make sure you include the file protocol (e.g. https://) and that NO TRAILING SLASH is included
-    "domain": "https://www.example.com"
+    "domain": "https://fission.digital"
 };
